e69aecd2536ba6edfe2c96f9f6d69dbce8aba572
caren1 <czarnocki.w@wp.pl>
2020-10-17 16:29:26 +0200

    initial api call, and form input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+// 
+
 function App() {
+
+  const [ coins, setCoins ] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then((res) => {
+      setCoins(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <h1>APII</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">
+          Search a currency
+        </h1>
+        <form>
+          <input type="text" className="coin-input" placeholder="Search"/>
+        </form>
+      </div>
+      {coins.map((coin) => {
+        // creating coin component
+      })}
     </div>
   );
 }
