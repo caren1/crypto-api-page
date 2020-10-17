b6c616923c4049c1841b45ba4ddd55023ca1ac18
caren1 <czarnocki.w@wp.pl>
2020-10-17 16:39:06 +0200

    constructed Coin component, rendered to the UI for each coin from API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
-// 
+import Coin from './Coin'
 
 function App() {
 
   const [ coins, setCoins ] = useState([]);
+  const [ search, setSearch ] = useState('');
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false')
@@ -18,6 +19,12 @@ function App() {
     })
   }, []);
 
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  }
+
+  const filteredCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div className="coin-app">
       <div className="coin-search">
@@ -25,12 +32,12 @@ function App() {
           Search a currency
         </h1>
         <form>
-          <input type="text" className="coin-input" placeholder="Search"/>
+          <input type="text" className="coin-input" placeholder="Search" onChange={handleChange}/>
         </form>
       </div>
-      {coins.map((coin) => {
-        // creating coin component
-      })}
+      {filteredCoins.map((coin) => (
+        <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} price={coin.current_price} volume={coin.market_cap}/>
+        ))}
     </div>
   );
 }
