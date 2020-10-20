f51b6871fa7616b5a798ef526d1c3f35d162c0df
caren1 <czarnocki.w@wp.pl>
2020-10-20 09:14:41 +0200

    Updates

---
+++ precache-manifest.7d7e21b7cfbb4ce626ea1479a50a92dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a58e0301d5db32e4c3c7c735097a1c",
+    "url": "/crypto-api-page/index.html"
+  },
+  {
+    "revision": "a51cbd707cf2e02e4e5a",
+    "url": "/crypto-api-page/static/css/main.7771a5e3.chunk.css"
+  },
+  {
+    "revision": "e23ea5a1d52b1015bf16",
+    "url": "/crypto-api-page/static/js/2.9eff60c2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/crypto-api-page/static/js/2.9eff60c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51cbd707cf2e02e4e5a",
+    "url": "/crypto-api-page/static/js/main.d4ec388d.chunk.js"
+  },
+  {
+    "revision": "02e7e98d5cd773d4b3d6",
+    "url": "/crypto-api-page/static/js/runtime-main.bcdd8832.js"
+  }
+]);
